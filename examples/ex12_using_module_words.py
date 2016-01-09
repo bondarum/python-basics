e6e2d9a1c35baf609b2672934c09cddec0128535
Mateusz Koncikowski <mkoncikowski@gmail.com>
2016-01-09 14:18:32 +0100

    Part 1 workshops

---
+++ examples/ex12_using_module_words.py
@@ -0,0 +1,24 @@
+import ex12_module_words
+
+sentence = "All good things come to those who wait."
+words = ex12_module_words.break_words(sentence)
+print words
+
+sorted_words = ex12_module_words.sort_words(words)
+print sorted_words
+
+ex12_module_words.print_first_word(words)
+ex12_module_words.print_last_word(words)
+print words
+
+ex12_module_words.print_first_word(sorted_words)
+ex12_module_words.print_last_word(sorted_words)
+print sorted_words
+
+sorted_words = ex12_module_words.sort_sentence(sentence)
+print sorted_words
+
+ex12_module_words.print_first_and_last(sentence)
+ex12_module_words.print_first_and_last_sorted(sentence)
+
+# Workshop time!

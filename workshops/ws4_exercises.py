777b1454626722b74da24f0a828b5fa6f23ddbad
Mateusz Koncikowski <mkoncikowski@gmail.com>
2016-01-15 09:59:46 +0100

    Add solutions

--- workshops/ws4_exercises.py
+++ workshops/ws4_exercises.py
@@ -21,20 +21,6 @@ def max_of_tree(x, y, z):
         return z
 
 
-# Define a function that computes the length of a given list or string.
-# (It is true that Python has the len() function built in, but writing it yourself is nevertheless a good exercise.)
-def my_len(string):
-    length = 0
-    is_string_finished = False
-    while not is_string_finished:
-        try:
-            string[length]
-            length += 1
-        except:
-            is_string_finished = True
-    return length
-
-
 # Write a function that takes a character (i.e. a string of length 1) and returns True if it is a vowel,
 # False otherwise.
 def is_vowel(character):
@@ -44,7 +30,7 @@ def is_vowel(character):
     else:
         return False
 
-# No. 5
+
 # Write a function translate() that will translate a text into "rövarspråket" (Swedish for "robber's language").
 # That is, double every consonant and place an occurrence of "o" in between.
 # For example, translate("this is fun") should return the string "tothohisos isos fofunon".
@@ -52,7 +38,6 @@ def translate(text_to_translate):
     pass
 
 
-# No. 6
 # Define a function my_sum() and a function multiply() that sums and multiplies (respectively)
 # all the numbers in a list of numbers. For example, sum([1, 2, 3, 4]) should return 10,
 # and multiply([1, 2, 3, 4]) should return 24.
@@ -69,20 +54,55 @@ def multiply(list_to_mulitiply):
         sum_of_multiply = sum_of_multiply * list_to_mulitiply[index]
     return sum_of_multiply
 
-# No. 7
+
 # Define a function reverse() that computes the reversal of a string.
 # For example, reverse("I am testing") should return the string "gnitset ma I".
+def reverse(string_to_reverse):
+    reversed_string = ''
+    for character in string_to_reverse[::-1]:
+        reversed_string = reversed_string + character
+    return reversed_string
 
-# No. 8
+
 # Define a function is_palindrome() that recognizes palindromes (i.e. words that look the same written backwards).
 # For example, is_palindrome("radar") should return True.
+def is_palindrome(string):
+    reversed_string = reverse(string)
+    if string == reversed_string:
+        return True
+    else:
+        return False
 
-# No. 9
+
 # Write a function is_member() that takes a value (i.e. a number, string, etc) x and a list of values a,
 # and returns True if x is a member of a, False otherwise. (Note that this is exactly what the in operator does,
 # but for the sake of the exercise you should pretend Python did not have this operator.)
+def is_member(value, list_to_check):
+    for list_value in list_to_check:
+        if list_value == value:
+            return True
+    return False
 
-# No. 10
+
 # Define a function overlapping() that takes two lists and returns True if they have at least one member in common,
 # False otherwise. You may use your is_member() function, or the in operator,
 # but for the sake of the exercise, you should (also) write it using two nested for-loops.
+def overlapping(original_list, list_to_check_for_overlapping):
+    for list_value in list_to_check_for_overlapping:
+        if is_member(list_value, original_list):
+            return True
+    return False
+
+
+# Define a function that computes the length of a given list or string.
+# (It is true that Python has the len() function built in, but writing it yourself is nevertheless a good exercise.)
+def my_len(string):
+    length = 0
+    is_string_finished = False
+    while not is_string_finished:
+        try:
+            string[length]
+            length += 1
+        except:
+            is_string_finished = True
+    return length
--- workshops/ws4_test_exercises.py
+++ workshops/ws4_test_exercises.py
@@ -1,6 +1,7 @@
 import unittest
 
-from workshops.ws4_exercises import my_max, max_of_tree, my_len, is_vowel, my_sum, multiply
+from workshops.ws4_exercises import my_max, max_of_tree, my_len, is_vowel, my_sum, multiply, reverse, is_palindrome, \
+    is_member, overlapping
 
 
 class TestPythonFunctions(unittest.TestCase):
@@ -13,12 +14,6 @@ class TestPythonFunctions(unittest.TestCase):
         self.assertEqual(max_of_tree(4, 6, 5), 6)
         self.assertEqual(max_of_tree(9, 8, 7), 9)
 
-    def test_my_len_function_should_return_expected_length_of_string(self):
-        self.assertEqual(my_len('test'), 4)
-        self.assertEqual(my_len('some sentence'), 13)
-        self.assertEqual(my_len(''), 0)
-        self.assertEqual(my_len('!@#$%^&*()'), 10)
-
     def test_is_vowel_function_should_true_if_given_character_is_vowel(self):
         self.assertTrue(is_vowel('a'))
         self.assertFalse(is_vowel('b'))
@@ -30,6 +25,31 @@ class TestPythonFunctions(unittest.TestCase):
     def test_multiply_function_should_multiply_given_numbers_in_list(self):
         self.assertEqual(multiply([2, 2, 3]), 12)
         self.assertEqual(multiply([100, 1]), 100)
+        self.assertEqual(multiply([15, 2, 4]), 120)
+
+    def test_reverse_function_should_return_reversed_string(self):
+        self.assertEqual(reverse('I am learning python'), 'nohtyp gninrael ma I')
+        self.assertEqual(reverse('Mateusz is a great trainer'), 'reniart taerg a si zsuetaM')
+
+    def test_is_palindrome_function_should_return_true_if_given_string_is_palindrome(self):
+        self.assertTrue(is_palindrome('level'))
+        self.assertTrue(is_palindrome('noon'))
+        self.assertFalse(is_palindrome('kasamdasak'))
+
+    def test_is_member_function_should_return_true_if_given_value_is_member_of_given_list(self):
+        self.assertTrue(is_member('member', [1, 'asd', 123, 'member']))
+        self.assertFalse(is_member(1234, [1, 'asd', 123, 'member']))
+
+    def test_overlapping_function_should_true_if_given_lists_are_overlapping_each_other(self):
+        self.assertTrue(overlapping([1, 'asd', 123, 'member'], ['asd', 123]))
+        self.assertTrue(overlapping([1, 'asd', 123, 'member'], ['member', ]))
+        self.assertFalse(overlapping([1, 'asd', 123, 'member'], ['member2']))
+
+    def test_my_len_function_should_return_expected_length_of_string(self):
+        self.assertEqual(my_len('test'), 4)
+        self.assertEqual(my_len('some sentence'), 13)
+        self.assertEqual(my_len(''), 0)
+        self.assertEqual(my_len('!@#$%^&*()'), 10)
 
 if __name__ == '__main__':
     unittest.main()

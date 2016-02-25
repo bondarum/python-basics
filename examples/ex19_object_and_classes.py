ad07201c5854ebec94ee24201db4f6e811d70588
Mateusz Koncikowski <mkoncikowski@gmail.com>
2016-02-25 20:45:07 +0100

    Add some description for oop

--- examples/ex19_object_and_classes.py
+++ examples/ex19_object_and_classes.py
@@ -19,6 +19,16 @@
 # has-a
 #   A phrase to say that something is composed of other things or has a trait, as in "a salmon has-a mouth."
 
+# A class definition can be compared to the recipe to bake a cake. A recipe is needed to bake a cake.
+# The main difference between a recipe (class) and a cake (an instance or an object of this class) is obvious.
+# A cake can be eaten when it is baked, but you can't eat a recipe, unless you like the taste of printed paper.
+# Like baking a cake, an OOP program constructs objects according to the class definitions of the program program.
+# A class contains variables and methods. If you bake a cake you need ingredients and instructions to bake the cake.
+# Accordingly a class needs variables and methods.
+# There are class variables, which have the same value in all methods and there are instance variables,
+# which have normally different values for different objects. A class also has to define all the necessary methods,
+# which are needed to access the data.
+
 
 class Animal(object):
     pass
